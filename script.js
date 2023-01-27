5d45e68a1f85c0f2f9cce903d33589e17888aab0
goAT2160 <118431904+goAT2160@users.noreply.github.com>
2023-01-27 19:48:45 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var button = document.getElementById("btn")
+var Rbutton = document.getElementById("rbtn")
+var h1 = document.getElementById("number")
+localStorage.setItem("prints", localStorage.getItem("prints"))
+
+h1.innerHTML = localStorage.getItem("prints")
+function minus() {
+    if (localStorage.getItem("prints") == "") {
+        localStorage.setItem("prints", "10")
+        localStorage.setItem("prints", localStorage.getItem("prints") - 1)
+        h1.innerHTML = localStorage.getItem("prints")
+        console.log(localStorage.getItem("prints"))
+    } else {
+        localStorage.setItem("prints", localStorage.getItem("prints") - 1)
+        h1.innerHTML = localStorage.getItem("prints")
+        console.log(localStorage.getItem("prints"))
+    }
+    
+}
+
+function reset() {
+    localStorage.setItem("prints", "10")
+    h1.innerHTML = localStorage.getItem("prints")
+}
+
+button.addEventListener('click', minus)
+Rbutton.addEventListener('click', reset)
